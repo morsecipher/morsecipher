b10e9786c6bf953b7ef28445d7d2178722d5fd26
Aidan Rudkovskyi <rrubyist@gmail.com>
2021-04-24 18:07:46 +0200

    Connect to the container mqtt instance

--- lib/morsecipher/verbalizer/adapter.ex
+++ lib/morsecipher/verbalizer/adapter.ex
@@ -9,9 +9,9 @@ defmodule Morsecipher.Verbalizer.Adapter do
 
   @spec beep(pid, non_neg_integer()) :: :ok
   def beep(_, times) do
-    {:ok, ref} = Tortoise.publish(Morsecipher, "morse/msg", "1", qos: 2)
+    {:ok, _ref} = Tortoise.publish(Morsecipher, "morse/msg", "1", qos: 2)
     :timer.sleep(@unit*times)
-    {:ok, ref} = Tortoise.publish(Morsecipher, "morse/msg", "0", qos: 2)
+    {:ok, _ref} = Tortoise.publish(Morsecipher, "morse/msg", "0", qos: 2)
     :ok
   end
 
--- lib/morsecipher/verbalizer/server.ex
+++ lib/morsecipher/verbalizer/server.ex
@@ -4,7 +4,7 @@ defmodule Morsecipher.Verbalizer.Server do
   def start_link(_opts) do
     Tortoise.Connection.start_link(
       client_id: Morsecipher,
-      server: {Tortoise.Transport.Tcp, host: "localhost", port: 1883},
+      server: {Tortoise.Transport.Tcp, host: "mosquitto", port: 1883},
       user_name: System.get_env("MOSQUITTO_USERNAME"),
       password: System.get_env("MOSQUITTO_PASSWORD"),
       handler: {Tortoise.Handler.Logger, []}
